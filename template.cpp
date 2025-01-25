213add33352ec37a428a9f010c3d22600d676d40
Renan Vieira <renanv.guedes7@gmail.com>
2025-01-25 14:30:04 -0300

    add comp float in template

--- template.cpp
+++ template.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 const int oo = 1e9;
+const double EPS{1e-9};
 
 using ll = long long;
 using ii = pair<int, int>;
@@ -9,6 +10,8 @@ using edge = tuple<int, int, int>;
 typedef vector<int> vi;
 #define pb push_back
 #define ln "\n"
+
+bool equals(long double a, long double b) { return fabs(a - b) < EPS; }
 
 int main() {
   ios::sync_with_stdio(0);
